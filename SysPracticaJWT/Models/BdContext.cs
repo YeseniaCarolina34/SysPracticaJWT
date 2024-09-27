732ca66679d4a880bad7db848d07d324a89970e1
MINEDUCYT <MINEDUCYT@YESENIA>
2024-09-26 19:20:24 -0600

    Agregar archivos de proyecto.

---
+++ SysPracticaJWT/Models/BdContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace SysPracticaJWT.Models;
+
+public partial class BdContext : DbContext
+{
+    public BdContext()
+    {
+    }
+
+    public BdContext(DbContextOptions<BdContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Rol> Rol { get; set; }
+
+    public virtual DbSet<Usuario> Usuario { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    { }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Rol>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Rol__3214EC07666A7BCC");
+        });
+
+        modelBuilder.Entity<Usuario>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Usuario__3214EC07FA8125FB");
+
+            entity.Property(e => e.Password).IsFixedLength();
+
+            entity.HasOne(d => d.IdRolNavigation).WithMany(p => p.Usuario)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK1_Rol_Usuario");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
